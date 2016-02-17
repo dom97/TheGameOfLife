ac09ece87b1a4663e94e7ac01d23d1d6b28267d8
Dom Edwards <dedwards@qualtrax.com>
2016-02-17 10:35:59 -0500

    Working game without ui

---
+++ GameOfLife/GameOfLife.Tests/WorldTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLife.Tests
+{
+    [TestClass]
+    public class WorldTests
+    {
+        [TestMethod]
+        public void AnyLiveCellWithFewerThanTwoLiveNeighboursDies()
+        {
+            var world = new World(4, 4);
+            world.BringToLife(1, 1);
+            world.Tick();
+
+            for (var i = 1; i <= 4; i++)
+                for (var j = 1; j <= 4; j++)
+                    Assert.IsFalse(world.IsAlive(i, j));
+        }
+
+        [TestMethod]
+        public void AnyLiveCellWithTwoLiveNeighborsLives()
+        {
+            var world = new World(4, 4);
+            world.BringToLife(1, 1);
+            world.BringToLife(2, 1);
+            world.BringToLife(1, 2);
+            world.Tick();
+
+            Assert.IsTrue(world.IsAlive(1, 1));
+            Assert.IsTrue(world.IsAlive(2, 1));
+            Assert.IsTrue(world.IsAlive(1, 2));
+        }
+
+        [TestMethod]
+        public void AnyLiveCellWithThreeLiveNeighborsLives()
+        {
+            var world = new World(4, 4);
+            world.BringToLife(1, 1);
+            world.BringToLife(2, 1);
+            world.BringToLife(1, 2);
+            world.BringToLife(2, 2);
+            world.Tick();
+
+            Assert.IsTrue(world.IsAlive(1, 1));
+            Assert.IsTrue(world.IsAlive(2, 1));
+            Assert.IsTrue(world.IsAlive(1, 2));
+            Assert.IsTrue(world.IsAlive(2, 2));
+        }
+
+        [TestMethod]
+        public void AnyLiveCellWithMoreThanThreeLiveNeighborsDies()
+        {
+            var world = new World(4, 4);
+            world.BringToLife(2, 2);
+            world.BringToLife(1, 1);
+            world.BringToLife(1, 2);
+            world.BringToLife(2, 3);
+            world.BringToLife(3, 2);
+            world.Tick();
+
+            Assert.IsFalse(world.IsAlive(2, 2));
+            Assert.IsTrue(world.IsAlive(1, 1));
+            Assert.IsTrue(world.IsAlive(1, 2));
+            Assert.IsTrue(world.IsAlive(2, 3));
+            Assert.IsTrue(world.IsAlive(3, 2));
+        }
+
+        [TestMethod]
+        public void AnyDeadCellWithExactlyThreeLiveNeighborsBecomesALiveCell()
+        {
+            var world = new World(4, 4);
+            world.BringToLife(1, 1);
+            world.BringToLife(1, 2);
+            world.BringToLife(2, 3);
+            world.Tick();
+
+            Assert.IsTrue(world.IsAlive(2, 2));
+            Assert.IsFalse(world.IsAlive(1, 1));
+            Assert.IsTrue(world.IsAlive(1, 2));
+            Assert.IsFalse(world.IsAlive(2, 3));
+        }
+    }
+}
---
+++ GameOfLife/GameOfLife/World.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace GameOfLife
+{
+    public class World
+    {
+        private Boolean[,] world;
+        private Boolean[,] nextGeneration;
+
+        public World(Int32 rows, Int32 columns)
+        {
+            world = new Boolean[rows, columns];
+            nextGeneration = new Boolean[rows, columns];
+        }
+
+        public void BringToLife(Int32 row, Int32 column)
+        {
+            world[row - 1, column - 1] = true;
+        }
+
+        public void Tick()
+        {
+            Array.Copy(world, nextGeneration, world.Length);
+
+            for (var i = 0; i < world.GetLength(0); i++)
+            {
+                for (var j = 0; j < world.GetLength(1); j++)
+                {
+                    var neighbors = LiveNeighbors(i, j);
+
+                    if (neighbors < 2 || neighbors > 3)
+                        nextGeneration[i, j] = false;
+                    else if (neighbors == 3)
+                        nextGeneration[i, j] = true;
+                }
+            }
+
+            Array.Copy(nextGeneration, world, nextGeneration.Length);
+        }
+
+        public Boolean IsAlive(Int32 row, Int32 column)
+        {
+            return world[row - 1, column - 1];
+        }
+
+        private Int32 LiveNeighbors(Int32 row, Int32 column)
+        {
+            var alive = 0;
+
+            for (var i = row - 1; i <= row + 1; i++)
+            {
+                for (var j = column - 1; j <= column + 1; j++)
+                {
+                    if (IsOutOfBounds(i, j) || IsSameCell(row, column, i, j))
+                        continue;
+
+                    if (world[i, j])
+                        alive++;
+                }
+            }
+
+            return alive;
+        }
+
+        private Boolean IsOutOfBounds(Int32 rowIndex, Int32 columnIndex)
+        {
+            return rowIndex < 0 || rowIndex >= world.GetLength(0) || columnIndex < 0 || columnIndex >= world.GetLength(1);
+        }
+
+        private static Boolean IsSameCell(Int32 row, Int32 column, Int32 rowIndex, Int32 columnIndex)
+        {
+            return rowIndex == row && columnIndex == column;
+        }
+    }
+}
